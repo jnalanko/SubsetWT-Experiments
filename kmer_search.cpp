04bf91afadd1f8062bb7e0307863991d2fde1d10
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-10-25 22:27:43 +0200

    Don't use streaming support to make results match the paper

--- kmer_search.cpp
+++ kmer_search.cpp
@@ -39,18 +39,19 @@ void benchmark(const variant_t& index, const string& index_name, const string& q
         int64_t len = reader.get_next_read_to_buffer();
         if(len == 0) break;
         int64_t t0 = current_time_micros();
-        vector<int64_t> results = index.streaming_search(reader.read_buf);
+	for(int64_t i = 0; i < len-index.get_k()+1; i++){
+            sum += index.search(reader.read_buf + i);
+            n_kmers++;
+        }
         int64_t t1 = current_time_micros();
         total_time_micros += t1 - t0; 
-        for(int64_t x : results) sum += x;
-        n_kmers += results.size();
     }
 
+    cerr << "Index kmers and subsets " << index.number_of_kmers() << " " << index.number_of_subsets() << endl;
     cerr << "Time: " << (double)(total_time_micros) / n_kmers << " us/kmer" << endl;
     cerr << "Sum: " << sum << endl;
 
     int64_t space_bits = index.get_subset_rank_structure().size_in_bytes() * 8;
-    space_bits += index.number_of_subsets(); // streaming support one bit per subset
     space_bits += index.get_precalc().size() * sizeof(pair<int64_t, int64_t>) * 8; // Precalc
     csv_out << index_name << ", " << (double)(total_time_micros) / n_kmers << ", " << (double)space_bits / index.number_of_kmers() << endl;
 
